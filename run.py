c7014a943e51f06c80373ac28b975b214a95477d
Tomasz <tomaszwoloszyn983@gmail.com>
2022-06-25 16:54:53 +0000

    Add validate_data function

--- run.py
+++ run.py
@@ -17,11 +17,47 @@ def get_sales_data():
     """
     Get sales figures input from the user
     """
-    print("Please enter sales data from the last market>")
-    print("Data should be six numbers, separated by commas")
-    print("Example: 10,20,30,40,50,60")
 
-    data_str = input("Enter your data here: ")
-    print(f"The data probided is {data_str}")
+    while True:
+        print("Please enter sales data from the last market>")
+        print("Data should be six numbers, separated by commas")
+        print("Example: 10,20,30,40,50,60")
 
-get_sales_data()
+        data_str = input("Enter your data here: ")
+        print(f"The data probided is {data_str}")
+
+        # Create a variable that stores the input split into single numbers divided by commas
+        sales_data = data_str.split(",")
+        print(sales_data) 
+
+        # If validate-data returns true break the loop, otherwise the loop repeats itself
+        if validate_data(sales_data):
+            print("Data is valid")
+            break
+
+    return sales_data
+
+def validate_data(values):
+    """
+    Inside the try, converts all string values into integers.
+    Raises ValueError if strings cannot be converted into int,
+    or if there aren't exactly 6 values.
+    Returns True when input data are correct.
+    """
+    try:
+        # Converts input data (which are strings) into integers
+        [int(value) for value in values]
+
+        # Validate the number of input elements
+        if len(values) != 6:
+            raise ValueError( 
+                f"Exactly 6 values required, you provided {len(values)}"
+            )
+    except ValueError as e:
+        print(f"Invalid data: {e}, please try again. \n")
+        return False
+    return True
+
+
+data = get_sales_data()
+print(data)
