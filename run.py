42c922f56d0d21e2c9c5b6b70dd5001d55151685
Tomasz <tomaszwoloszyn983@gmail.com>
2022-06-26 07:03:42 +0000

    Update calculate_surplus_data function, calculate surplus

--- run.py
+++ run.py
@@ -60,14 +60,56 @@ def validate_data(values):
 
 def update_sales_worksheet(data):
     """
-    Update salws worksheet, add new row with the list data provided.
+    Update sales worksheet, add new row with the list data provided
     """
-    print("Update sales worksheet... \n")
+    print("Updating sales worksheet...\n")
     sales_worksheet = SHEET.worksheet("sales")
     sales_worksheet.append_row(data)
-    print("Sales worksheet update successfully.\n")
+    print("Sales worksheet updated successfully.\n")
 
-data = get_sales_data()
-print(data)
-sales_data = [int(num) for num in data]
-update_sales_worksheet(sales_data)
+
+def calculate_surplus_data(sales_row):
+    """
+    Compare sales with stock and calculate the surplus for each item type.
+    The surplus is defined as the sales figure subtracted from the stock:
+    - Positive surplus indicates waste
+    - Negative surplus indicates extra made when stock was sold out.
+
+    Open connection to the worksheet.
+    Get the last row of data in the sheet (stock[-1]). Index -1 
+    is the first index counting from the last
+    """
+    print("Calculating surplus data...\n")
+    stock = SHEET.worksheet("stock").get_all_values()
+    stock_row = stock[-1]
+    print(stock_row)
+
+    # Zip method allows us to interate dwo iterable structures at a time.
+    # Here we iterate our stocklist and subtract the number of sold items 
+    # Then we add remaining walue to the worksheet
+    surplus_data = []
+    for stock, sales in zip(stock_row, sales_row):
+        surplus = int(stock) - sales
+        surplus_data.append(surplus)
+    print(surplus_data)
+
+
+
+
+def main():
+    """
+    Run all program function
+    """
+    data = get_sales_data()
+    sales_data = [int(num) for num in data]
+    update_sales_worksheet(sales_data)
+    new_surplus_data = calculate_surplus_data(sales_data)
+    print(new_surplus_data)
+
+# main is the starting function. It can be called anyehere in the code, the only 
+# contition is that it has to be called below it is declared.
+# Interestingly the print statement that is just abouve the main() function
+# is going to be displayed before the main function.
+
+print("Welcome to love sandwiches Data Automation")
+main()
